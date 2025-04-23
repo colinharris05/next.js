1cd26f8e925f237d738f7c862c6b88fa5572d87a
Janka Uryga <lolzatu2@gmail.com>
2025-04-23 20:22:04 +0200

    tests for tracking dynamic imports

---
+++ test/e2e/app-dir/dynamic-io-dynamic-imports/dynamic-io-dynamic-imports.test.ts
@@ -0,0 +1,97 @@
+import { nextTestSetup } from 'e2e-utils'
+
+describe('async imports in dynamicIO', () => {
+  const { next, isNextStart } = nextTestSetup({
+    files: __dirname,
+  })
+
+  if (isNextStart) {
+    it('does not cause any routes to become dynamic', async () => {
+      const prerenderManifest = JSON.parse(
+        await next.readFile('.next/prerender-manifest.json')
+      )
+      let prerenderedRoutes = Object.keys(prerenderManifest.routes).sort()
+      expect(prerenderedRoutes).toEqual([
+        '/inside-client-component/async-module',
+        '/inside-client-component/sync-module',
+        '/inside-component/async-module',
+        '/inside-component/from-node-modules/cjs/sync-module',
+        '/inside-component/from-node-modules/esm/async-module',
+        '/inside-component/from-node-modules/esm/sync-module',
+        '/inside-component/sync-module',
+        '/inside-route-handler/async-module',
+        '/inside-route-handler/sync-module',
+      ])
+    })
+  }
+
+  describe('inside a server component', () => {
+    it('import of a sync module', async () => {
+      const browser = await next.browser('/inside-component/sync-module')
+      expect(await browser.elementByCss('body').text()).toBe('hello')
+    })
+
+    it('import of module with top-level-await', async () => {
+      const browser = await next.browser('/inside-component/async-module')
+      expect(await browser.elementByCss('body').text()).toBe('hello')
+    })
+
+    describe('dynamic import in node_modules', () => {
+      describe('in an ESM package', () => {
+        it('import of a sync module', async () => {
+          const browser = await next.browser(
+            '/inside-component/from-node-modules/esm/sync-module'
+          )
+          expect(await browser.elementByCss('body').text()).toBe('hello')
+        })
+
+        it('import of module with top-level-await', async () => {
+          const browser = await next.browser(
+            '/inside-component/from-node-modules/esm/async-module'
+          )
+          expect(await browser.elementByCss('body').text()).toBe('hello')
+        })
+      })
+
+      describe('in a CJS package', () => {
+        // CJS can't do top-level-await, so we're only testing sync modules
+        it('import of a sync module', async () => {
+          const browser = await next.browser(
+            '/inside-component/from-node-modules/cjs/sync-module'
+          )
+          expect(await browser.elementByCss('body').text()).toBe('hello')
+        })
+      })
+    })
+  })
+
+  describe('inside a client component', () => {
+    it('import of a sync module', async () => {
+      const browser = await next.browser('/inside-client-component/sync-module')
+      expect(await browser.elementByCss('body').text()).toBe('hello')
+    })
+
+    it('import of module with top-level-await', async () => {
+      const browser = await next.browser(
+        '/inside-client-component/async-module'
+      )
+      expect(await browser.elementByCss('body').text()).toBe('hello')
+    })
+  })
+
+  describe('inside a GET route handler', () => {
+    it('import of a sync module', async () => {
+      const result = await next
+        .fetch('/inside-route-handler/sync-module')
+        .then((res) => res.text())
+      expect(result).toBe('hello')
+    })
+
+    it('import of module with top-level-await', async () => {
+      const result = await next
+        .fetch('/inside-route-handler/async-module')
+        .then((res) => res.text())
+      expect(result).toBe('hello')
+    })
+  })
+})
